51c1fcfc35cb90b06a3a4f5cc02d9e87585e8893
sandhyapatil <sandhya2088@gmail.com>
2025-05-31 16:14:44 -0700

    Configure for GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/sandhya-blog',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
