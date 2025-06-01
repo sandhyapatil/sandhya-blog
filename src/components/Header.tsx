32b4bd02e0e2b06c3e736938d667b61e03893026
sandhyapatil <sandhya2088@gmail.com>
2025-05-31 20:31:18 -0700

    Replace site title with home icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,10 @@ export default function Header() {
   return (
     <header className="py-8">
       <nav className="flex items-center justify-between">
-        <Link href="/" className="text-2xl font-bold">
-          Sandhya's Blog
+        <Link href="/" className="text-2xl hover:text-gray-600 dark:hover:text-gray-300 transition-colors">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+          </svg>
         </Link>
         <ul className="flex space-x-6">
           <li>
