5bc5a074e0f03eca80bc24839b2220b4ffee4dd5
sandhyapatil <sandhya2088@gmail.com>
2025-05-31 20:23:25 -0700

    Remove sidebar navigation

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,10 +1,7 @@
-import Sidebar from './Sidebar';
-
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-white dark:bg-gray-900">
-      <Sidebar />
-      <main className="pl-64">
+      <main>
         <div className="max-w-4xl mx-auto px-4 py-8">
           {children}
         </div>
